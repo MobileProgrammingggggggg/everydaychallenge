d92e8355c907d912c0cbea4cb1dd415c12c46b02
kmj <119595705+skwnddp@users.noreply.github.com>
2024-11-14 00:37:05 +0900

    파이어베이스 설정 파일만 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'Main_test.dart';
 
 //main 파일 이름 변경, 삭제 노노
-void main() {
+void main() async  {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
